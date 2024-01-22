25194d57696ff58999bee3a7f55cca9cb3adc084
sabiaparveen1 <sabia.parveen@nashtechglobal.com>
2024-01-22 22:52:30 +0530

    inial commit

---
+++ src/main/java/com/cloudevents/cloudeventsdemo/CloudeventsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cloudevents.cloudeventsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeventsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudeventsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudevents/cloudeventsdemo/CloudeventsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudevents.cloudeventsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudeventsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
